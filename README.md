6366756609059a3f583912194396ff2197d5c189
Michael Loubier <michael.loubier@students.makeschool.com>
2016-10-18 22:26:46 -0700

    initial commit with framework files

---
+++ README.md
@@ -0,0 +1,1 @@
+# hassurunous.github.io
---
+++ formatting.js
@@ -0,0 +1,12 @@
+function onload() {
+    var subtitles = document.getElementsByClassName("boxsubtitle");
+    console.log(JSON.stringify(subtitles));
+    var boxes = [];
+    var titles = [];
+    for (var i = 0; i < subtitles.length; i++) {
+        //boxes.append(subtitles[i].parentNode);
+    }
+    for (var i = 0; i < boxes.length; i++) {
+
+    }
+}
