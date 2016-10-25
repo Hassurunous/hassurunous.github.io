89c5b83ea56a6b48c860ebb2d5de2f34840b8bc6
Michael Loubier <michael.loubier@students.makeschool.com>
2016-10-24 20:07:28 -0700

    Working on a cooler 2nd version of the website. Testing rotation effects.

---
+++ website2/js/data.js
@@ -0,0 +1,17 @@
+var webContent = [
+    {
+        id: "aboutme",
+        content: "Grew up in towns between Illinois, Michigan, Georgia, and Indiana, but calls his Hometown Leesburg, GA. Moved out to California in 2016 to pursue an opportunity to attend Make School to further his passion for helping people, having fun, and trying to make the world a better place for at least one person every day. He loves to play board games and video games with friends, listening to music, attending concerts, and exploring new lands. His ultimate goal would be to work at a company like Blizzard, Inc., Riot Games, Bioware, or Green Ronin. His ultimate desire is to better the world by using games as a tool to promote self-awareness, empathy, and critical thought."
+        background-color: "green";
+        
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+];
